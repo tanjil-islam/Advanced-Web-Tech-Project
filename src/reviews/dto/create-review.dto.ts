4323c6a7a365d297047301eaf09e280bfd6da877
tanjil-islam <tanjil.islam040820@gmail.com>
2024-12-21 04:49:38 +0600

    ReviewEntity Added for collecting reviews by users

---
+++ src/reviews/dto/create-review.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateReviewDto {
+    @IsNotEmpty({message:'Product should not be empty.'})
+    @IsNumber({},{message:'Product Id should be number.'})
+    productId:number;
+
+    @IsNotEmpty({message:'ratings should not be empty.'})
+    @IsNumber()
+    ratings:number;
+
+    @IsNotEmpty({message:'comment should not be empty.'})
+    @IsString()
+    comment:string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { CategoryEntity } from "src/categories/entities/category.entity";
 import { ProductEntity } from "src/products/entities/product.entity";
+import { ReviewEntity } from "src/reviews/entities/review.entity";
 import { Roles } from "src/utility/common/user-roles.enum";
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
 
@@ -25,4 +26,7 @@ export class UserEntity{
     
     @OneToMany(()=>ProductEntity,(prod)=>prod.addedBy)
     products:ProductEntity[];
+
+    @OneToMany(()=> ReviewEntity,(rev)=>rev.user)
+    reviews:ReviewEntity[];
 }

9f4bcd500964d3ca1b8a1afaeeff6fb0bd866072
tanjil-islam <tanjil.islam040820@gmail.com>
2024-12-21 06:07:31 +0600

    complete all functionalies about reviews section.

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { AuthenticationGuard } from 'src/utility/common/guards/authentication.guard';
+import { CurrentUser } from 'src/utility/common/decorators/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { ReviewEntity } from './entities/review.entity';
+import { AuthorizeGuard } from 'src/utility/common/guards/authorization.guard';
+import { Roles } from 'src/utility/common/user-roles.enum';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
-  @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  //@UseGuards(AuthenticationGuard)
+  @Post('addReview')
+  async create(@Body() createReviewDto: CreateReviewDto,@CurrentUser() currentUser:UserEntity):Promise<ReviewEntity> {
+    return await this.reviewsService.create(createReviewDto,currentUser);
   }
 
   @Get()
@@ -17,9 +24,14 @@ export class ReviewsController {
     return this.reviewsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+  @Get('byProductId')
+  async findAllByProduct(@Body('productId') productId:number){
+    return await this.reviewsService.findAllByProduct(+productId);
+  }
+
+  @Get('single/:id')
+ async findOne(@Param('id') id: string):Promise<ReviewEntity> {
+    return await this.reviewsService.findOne(+id);
   }
 
   @Patch(':id')
@@ -27,7 +39,8 @@ export class ReviewsController {
     return this.reviewsService.update(+id, updateReviewDto);
   }
 
-  @Delete(':id')
+  //@UseGuards(AuthenticationGuard,AuthorizeGuard([Roles.ADMIN]))
+  @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.reviewsService.remove(+id);
   }
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { ProductsModule } from 'src/products/products.module';
+import { ProductEntity } from 'src/products/entities/product.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewEntity } from './entities/review.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ReviewEntity]),ProductsModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
